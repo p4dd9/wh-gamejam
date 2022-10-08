3eae1731f28759b90e630c6cbd77a932214780ec
Patrick Obermueller <patrick.obermueller@willhaben.at>
2022-10-08 10:33:05 +0200

    chore: add airconsole script

--- app/js/controller/controller.js
+++ app/js/controller/controller.js
@@ -1,3 +1,5 @@
 import '../../css/controller.css'
 
+const airconsole = new AirConsole()
 console.log('controller.js')
+console.log(airconsole)
--- app/js/screen/screen.js
+++ app/js/screen/screen.js
@@ -1,3 +1,5 @@
 import '../../css/screen.css'
 
+const airconsole = new AirConsole()
 console.log('screen.js')
+console.log(airconsole)
