7753952ef439fa9e7c071c00124b2e6792fc81e7
Bernhard Kaszt <bernhard.kaszt@willhaben.at>
2022-10-12 15:58:27 +0200

    Remove unused controller.ts functions

--- app/ts/controller/controller.ts
+++ app/ts/controller/controller.ts
@@ -20,33 +20,6 @@ rightButton.addEventListener("click", () => {
 	moveRight()
 })
 
-/*
-* Checks if this device is part of the active game.
-*/
-airconsole.onActivePlayersChange = function (player) {
-	var div = document.getElementById("player_id")
-
-	if (div == null) {
-		return
-	}
-
-	if (player !== undefined) {
-		div.innerHTML =  (["Left Player", "Right Player"][player])
-	} else {
-		div.innerHTML = "It's a 2 player game!"
-	}
-}
-
-/*
-* Makes the device vibrate if the screen says so.
-*/
-airconsole.onMessage = function (from, data) {
-	if (from == AirConsole.SCREEN && data.vibrate) {
-		navigator.vibrate(data.vibrate)
-		console.log("Vibrating: " + data.vibrate)
-	}
-}
-
 /**
 * Tells the screen to move the player to the left.
 */
