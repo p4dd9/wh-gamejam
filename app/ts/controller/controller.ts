f90a39375547bfcac8896b80c4bc9b7b5bf69cc9
Patrick Obermueller <patrick.obermueller@willhaben.at>
2022-10-12 19:47:58 +0200

    feature: force controller into landscape move

--- app/ts/controller/controller.ts
+++ app/ts/controller/controller.ts
@@ -5,31 +5,32 @@
 import '../../css/controller.css'
 
 const airconsole = new AirConsole()
+airconsole.setOrientation(AirConsole.ORIENTATION_LANDSCAPE)
 const body = document.body
 
 /**
  * SAMPLE: send message to SCREEN
  */
-let leftButton = <HTMLElement>document.body.querySelector(".left")
-leftButton.addEventListener("click", () => {
+let leftButton = <HTMLElement>document.body.querySelector('.left')
+leftButton.addEventListener('click', () => {
 	moveLeft()
 })
 
-let rightButton = <HTMLElement>document.body.querySelector(".right")
-rightButton.addEventListener("click", () => {
+let rightButton = <HTMLElement>document.body.querySelector('.right')
+rightButton.addEventListener('click', () => {
 	moveRight()
 })
 
 /**
-* Tells the screen to move the player to the left.
-*/
+ * Tells the screen to move the player to the left.
+ */
 function moveLeft() {
-	airconsole.message(AirConsole.SCREEN, {MOVE: 'left'})
+	airconsole.message(AirConsole.SCREEN, { MOVE: 'left' })
 }
 
 /**
-* Tells the screen to move the player to the right.
-*/
+ * Tells the screen to move the player to the right.
+ */
 function moveRight() {
-	airconsole.message(AirConsole.SCREEN, {MOVE: 'right'})
+	airconsole.message(AirConsole.SCREEN, { MOVE: 'right' })
 }
